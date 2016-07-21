5b8b5c054f9132038d99545032cde8c7de6310ae
mustajabk <mustajabk78@gmail.com>
2016-07-20 19:08:01 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
+// written code insde a IFFE to create namespace and avoid scoping issues
+// uses john papa style guide 
+//uses tod motto module reveal patteren and controllerAs syntax
+//usually in angular js data comes from a service, should not be inside a controller
+
+(function() {
+  angular
+  	.module("app", [])
+  	.controller('TodoCtrl', TodoCtrl);
+  
+  function TodoCtrl() {
+  	var vm = this;
+    vm.todos = [
+      {text: 'HTML5', status: true},
+      {text: 'CSS3', status: false}
+    ];
+    
+    vm.addTodo = function() {
+      vm.todos.push({text: vm.todoText, status: false});
+      vm.todoText = '';
+    };
+
+    vm.archive = function() {
+      var oldTodos = vm.todos;
+      vm.todos = [];
+      angular.forEach(oldTodos, function(todo) {
+        if (!todo.status) vm.todos.push(todo);
+      });
+    };
+  }
+
+})();
+
 
